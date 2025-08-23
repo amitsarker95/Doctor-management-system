5b14dea524e3e5c6f52c80148e6da90a231262af
amit <amitsarker95@gmail.com>
2025-08-23 15:39:14 +0600

    Project initialized with core Custom user model

---
+++ doc_management/core/models.py
@@ -0,0 +1,43 @@
+from django.db import models
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, PermissionsMixin
+
+# Create your models here.
+
+
+class CustomUserManager(BaseUserManager):
+    def create_user(self, email, password=None, **extra_fields):
+        if not email:
+            raise ValueError("User must need an email..!")
+        email = self.normalize_email(email)
+        user = self.model(email, **extra_fields )
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+    
+    def create_super_user(self, email, password=None, **extra_fields):
+        extra_fields.setdefault("is_staff", True)
+        extra_fields.setdefault("is_superuser", True)
+
+        if extra_fields.get("is_staff") is not True:
+            raise ValueError("Super user must have is_staff=True")
+        if extra_fields.get("is_superuser") is not True:
+            raise ValueError("Super user must have is_superuser=True")
+        
+        return self.create_user(email, password, **extra_fields)
+    
+
+class CustomUser(AbstractBaseUser, PermissionsMixin):
+    email = models.EmailField(unique=True)
+    name = models.CharField(max_length=255)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+
+    objects = CustomUserManager()
+
+    USERNAME_FEILD = 'email'
+    REQUIRED_FIELDS = [
+
+    ]
+
+    def __str__(self):
+        return f"USER : {self.email}"
